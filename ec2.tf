528cd9109b19adb167c9464c08311fdcdb47dada
Kiran Kumar Kosuri <kosurikirankumar7@gmail.com>
2026-02-23 16:58:44 +0530

    Add EC2 module with instance configuration and outputs
    Added a module for EC2 instance configuration with outputs for public and private IPs.

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,23 @@
+module "catalogue" {
+  source = "../terraform-ec2-module-development"
+  ami_id = "ami-0220d79f3f480ecf5"
+  sg_ids = ["sg-0a6dfc4b214d67e82"]
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Module-Demo"
 
+
+ami_id = var.ami_id
+sg_ids = var.sg_ids
+instance_type = var.instance_type
+tags = var.tags
+}
+
+output "public_ip" {
+  value = module.catalogue.public_ip
+}
+
+output "private_ip" {
+  value = module.catalogue.private_ip
+}
+  
